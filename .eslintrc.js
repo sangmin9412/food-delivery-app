80d2ea1d14c79e3c6262455fa01a10376b50e7f0
sanmgin9412 <tkdals9048@naver.com>
2024-05-04 05:15:59 +0900

    Update: react-navigation setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react-native/no-inline-styles': 0,
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      env: {
+        browser: true,
+      },
+      rules: {'react-native/no-inline-styles': 0},
+    },
+  ],
 };
